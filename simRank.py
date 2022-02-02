428b8dc60530768c09b291de23fb3747fe404723
Georgia <47317062+gebaltso@users.noreply.github.com>
2022-02-02 11:30:35 +0200

    Update simRank.py

--- simRank.py
+++ simRank.py
@@ -20,83 +20,24 @@ from numpy import array
 import matplotlib.pyplot as plt
 
 
-##change dir
-#os.chdir('seperatedExps/datasets/lfr/')
-#
-##myFile is the input csv file
-##myFile = 'lfrEdgelistN1000MU0.1*.csv'
-##myFile = 'lfrEdgelistN5000MU0.40*.csv'
-##myFile = 'karate.csv'
-#myFile = 'lfr1.csv'
-##myFile = 'youTube.csv'
-##myFile = 'dblp.csv'
-##myFile = 'NetCol.csv'
-##myFile = 'agilentPearson6.csv'
-##myFile = 'Physical_Interactions.IREF-BIOGRID.csv'
-#
-##file is the input file with the LFR parameters in its name, in string format(without .csv)
-#file = myFile[:-4]
-#
-#l = 10
-#
-##copy input csv file to the weighted folder in order to run the experiments with the initial file too
-##shutil.copy2(myFile, '../weighted/'+str(file)+'<11111-11111>1.csv' )
-#
-##community file
-#communityFile = '/Users/georgiabaltsou/Desktop/PhD/Local_exp/seperatedExps/datasets/lfr/communityFile.txt'
-#
-##seeds = read seed nodes from seedFile
-#seedFile = open('/Users/georgiabaltsou/Desktop/PhD/Local_exp/seperatedExps/datasets/lfr/seedsetFile.txt', 'r')
-#seeds = seedFile.readline().split(" ")
-#seedsetFile = '/Users/georgiabaltsou/Desktop/PhD/Local_exp/seperatedExps/datasets/lfr/seedsetFile.txt'
-#
-##keep as seed the 1st seed of seedFile
-#seed = seeds[0]
-
-
-
-
-#Creation of the graph with the input file
-#G = nx.read_weighted_edgelist(myFile, create_using=nx.Graph(), delimiter=";", encoding='utf-8-sig')
-#Graph = defaultdict(dict)
-#with open(myFile, 'r') as read_file: 
-#    reader = csv.reader(read_file, delimiter=';')
-#    for row in reader:
-##        if row[0] in seeds or row[1] in seeds:
-#            Graph[row[0]][row[1]] = row[2] 
-#            Graph[row[1]][row[0]] = row[2]
-##G = nx.Graph(Graph)
-#
-#newGraph = {k: {kk: float(vv) for kk, vv in v.items()}
-#         for k, v in Graph.items()}
-#
-#
-#    
-#G = nx.Graph(newGraph)
-#
-#
-#for source, target in G.edges():
-#    G[source][target]['weight'] = newGraph[source][target]
-#    
-#print("------------------------------")
 
 def simRank(seeds, G, newGraph, hops, reWire):
-    C = nx.Graph() #dimiourgw arxika keno grafo
+    C = nx.Graph() #creation of an empty graph
     
-    for s in seeds: # gia ka8e seed kataskeuazw ton ypografo tou ws kai depth=hops
+    for s in seeds: # for each seed I create its subgraph till depth = hops
         tmp = nx.ego_graph(G, s, radius=hops if reWire else 1)
-        C = nx.compose(C, tmp) #enwnw ta epimerous grafhmata. px an oi seed einai 2 enwnw tous 2 ypografous se enan eniaio wste na mhn exw 2typa. Ton C
+        C = nx.compose(C, tmp) #merge the individual graphs. e.g. if |seed| = 2 I merge the 2 subgraphs in one such that I don't have doubles.
 
-#    print("Nodes=", len(C.nodes()), "Edges=", len(C.edges()))
+
 
     if reWire:
         
-        for n in seeds: #trexw ton simrank gia olous tous komvous tou C. Allazw ola ta barh metaksu olwn twn komvwn tou C
+        for n in seeds: #run simrank for all nodes in C. Change all the node weights in C.
             sim = nx.simrank_similarity(C, n, target=None, importance_factor=0.8, max_iterations=10, tolerance=0.0001)
             avg_sim = sum(sim.values()) / len(sim.values())
             for m in sim.keys():
                 if n == m or avg_sim > sim[m]: continue
-                newGraph[n][m] = 1 #allazw ta barh sto original grafo!!!!
+                newGraph[n][m] = 1 #change the weights of original graph!
                 newGraph[m][n] = 1
                 
         G = nx.Graph(newGraph) # rewiring
